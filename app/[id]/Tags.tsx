422fcc2a9c370bbbeef92005cda37bce10301e7d
Dillon <dillonmorris91@gmail.com>
2023-08-20 11:07:08 -0700

    Add remove tags, fetch from API

--- app/[id]/Tags.tsx
+++ app/[id]/Tags.tsx
@@ -4,6 +4,7 @@ import useSWR from 'swr'
 import Link from 'next/link'
 import { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { lightningcss } from 'tailwindcss/src/oxide/cli/build/deps'
 
 // @ts-ignore
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
@@ -18,36 +19,47 @@ export const Tags = () => {
     tags
   )
 
-  const { data, error, isLoading } = useSWR(apiUrl, fetcher)
+  const { data, isLoading } = useSWR(apiUrl, fetcher)
   const nextQuoteId = !isLoading && data[0]?._id
   const nextUrl = buildUrlWithTagsParam(`/${nextQuoteId}`, tags)
 
-  const handleAddTag = (tag: string) => {
-    setTags((tags) => [...tags, tag])
+  const handleTagClick = (tag: string) => {
+    const doesTagExist = tags.indexOf(tag) > -1
+    const removeOrAddTag = (tags) => {
+      if (doesTagExist) {
+        return tags.filter((t) => t !== tag)
+      }
+
+      return [...tags, tag]
+    }
+
+    setTags(removeOrAddTag)
   }
+
+  const tagsData = useTags()
 
   return (
     <div className="flex justify-between">
       <button
-        onClick={() => handleAddTag('History')}
+        onClick={() => handleTagClick('History')}
         className="inline-flex items-center rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-400/20"
       >
         History
       </button>
       <button
-        onClick={() => handleAddTag('Technology')}
+        onClick={() => handleTagClick('Technology')}
         className="inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-xs font-medium text-red-400 ring-1 ring-inset ring-red-400/20"
       >
         Technology
       </button>
       <button
-        onClick={() => handleAddTag('Love')}
+        onClick={() => handleTagClick('Love')}
         className="inline-flex items-center rounded-md bg-yellow-400/10 px-2 py-1 text-xs font-medium text-yellow-500 ring-1 ring-inset ring-yellow-400/20"
       >
         Love
       </button>
       <button
-        onClick={() => handleAddTag('Happiness')}
+        onClick={() => handleTagClick('Happiness')}
         className="inline-flex items-center rounded-md bg-green-500/10 px-2 py-1 text-xs font-medium text-green-400 ring-1 ring-inset ring-green-500/20"
       >
         Happiness
@@ -69,6 +81,31 @@ export const Tags = () => {
       <Link href={nextUrl}>Go to quote</Link>
     </div>
   )
+}
+
+const COLORS = [
+  'gray',
+  'red',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'purple',
+  'pink',
+]
+
+const useTags = () => {
+  const { data: tagsData } = useSWR('https://api.quotable.io/tags', fetcher)
+  const filteredTags = tagsData?.filter((tag) => tag.quoteCount > 10) || []
+  const colorsRepeatCount = Math.round(filteredTags.length / COLORS.length) - 1
+  const colorsRepeated = COLORS.concat(...Array(colorsRepeatCount).fill(COLORS))
+
+  const tags = filteredTags.reduce((acc, { name }, i) => {
+    const color = colorsRepeated[i]
+    return [...acc, { name, color }]
+  }, [])
+
+  return tags
 }
 
 const buildUrlWithTagsParam = (url, tags) => {
